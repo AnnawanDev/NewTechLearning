8844d5da1dbd9e83912ddf6906d74e8e674e515f
Ed Wied <wied.ed@gmail.com>
2021-03-06 11:38:17 -0600

    add more function documenation

--- src/routers/apiEndpoints.js
+++ src/routers/apiEndpoints.js
@@ -2,7 +2,7 @@
    CS 340 Final Project: New Tech Learning
    Nora Marji
    Ed Wied
-   February 17, 2021
+   March 6, 2021
 */
 
 const express = require('express');
@@ -16,6 +16,7 @@ const {requireAdminLogin,requireLogin} = require('../middleware/auth');
 //--------------------------------------------------------
 //---------------------- AUTHENTICATION ----------------------
 //--------------------------------------------------------
+// API endpoint to check login
 router.post('/api/login', async (req, res, next) => {
     if (!req.body['username'] || !req.body['password']) {
       logIt('ERROR! username and/or password not in POST body');
@@ -52,7 +53,7 @@ router.post('/api/login', async (req, res, next) => {
 //--------------------------------------------------------
 //---------------------- CATEGORIES ----------------------
 //--------------------------------------------------------
-
+// API endpoint to get list of available categories
 router.get('/api/getListOfAvailableCategories', async (req,res,next) => {
   try {
     let context = {};
@@ -71,6 +72,7 @@ router.get('/api/getListOfAvailableCategories', async (req,res,next) => {
   }
 });
 
+// API endpoint to get list of all categories
 router.get('/api/getListOfAllCategories', async (req,res,next) => {
   try {
     let context = {};
@@ -89,7 +91,7 @@ router.get('/api/getListOfAllCategories', async (req,res,next) => {
   }
 });
 
-//deletes a category from DB
+// API endpoint to delete a category from DB
 router.get('/api/deleteCategory/:categoryId', async (req,res,next) => {
   let context = {};
   mysql.pool.query("DELETE FROM `Categories` WHERE `categoryId` = ?", req.params.categoryId, (error, results, fields) => {
@@ -106,6 +108,7 @@ router.get('/api/deleteCategory/:categoryId', async (req,res,next) => {
   })
 });
 
+// API endpoint to insert a particular category
 router.get('/api/insertCategory/:categoryName', async (req,res,next) => {
   let context = {};
   mysql.pool.query("INSERT INTO `Categories` (`categoryName`) VALUES (?)", req.params.categoryName, (error, results, fields) => {
@@ -119,6 +122,7 @@ router.get('/api/insertCategory/:categoryName', async (req,res,next) => {
   });
 });
 
+// API endpoint to get list of all categories
 router.get('/api/getListOfAllCategories', async (req,res,next) => {
   let context = {};
 
@@ -135,6 +139,7 @@ router.get('/api/getListOfAllCategories', async (req,res,next) => {
     return context;
 });
 
+// API endpoint to get a particular category that's set for a course
 router.get('/api/getCategoryNameForCourse/:courseId', async (req,res,next) => {
   let context = {};
 
@@ -151,7 +156,7 @@ router.get('/api/getCategoryNameForCourse/:courseId', async (req,res,next) => {
     return context;
 });
 
-
+// API endpoint to update a category name
 router.post('/api/updateCategory/', async(req,res,next) => {
   let context = {};
   let filter = [req.body.categoryName, req.body.categoryId]
@@ -169,6 +174,7 @@ router.post('/api/updateCategory/', async(req,res,next) => {
 //--------------------------------------------------------
 //---------------------- COURSES ------------------
 //--------------------------------------------------------
+// API endpoint to get distinct list of courses
 router.get('/api/getCourses', async (req,res,next) => {
   let context = {};
 
@@ -194,29 +200,7 @@ router.get('/api/getCourses', async (req,res,next) => {
     return context;
 });
 
-router.get('/api/getCourseOverview/:courseName', async (req,res,next) => {
-  let context = {};
-
-  if (!req.params.courseName) {
-    logIt('ERROR! No courseName sent to /api/getCourseOverview/:courseName');
-    res.status(400).send();
-    return;
-  }
-
-  mysql.pool.query('SELECT `courseName` FROM `Courses`;', (err, rows, fields) => {
-      if (err) {
-        logIt("ERROR FROM /api/getCourseOverview/:courseName: " + err);
-        return;
-      }
-
-      logIt("/api/getCourseOverview/:courseName: " + JSON.stringify(rows));
-      context.results = rows;
-      res.send(context);
-    });
-    return context;
-});
-
-
+// API endpoint to get the 3 most recently added classes
 router.get('/api/selectMostRecentAddedClasses', async (req,res,next) => {
   try {
     let context = {};
@@ -237,6 +221,7 @@ router.get('/api/selectMostRecentAddedClasses', async (req,res,next) => {
   }
 });
 
+// API endpoint to get students enrolled in a particular class
 router.get('/api/getStudentsInClasses/:someClassId', requireLogin, async (req,res,next) => {
   try {
     let context = {};
@@ -260,7 +245,7 @@ router.get('/api/getStudentsInClasses/:someClassId', requireLogin, async (req,re
 //--------------------------------------------------------
 //---------------------- COURSE MODULES ------------------
 //--------------------------------------------------------
-
+// API endpoint to get course modules for a particular course ID
 router.get('/api/getModulesForCourse/:courseId', async (req,res,next) => {
   let context = {};
 
@@ -276,7 +261,7 @@ router.get('/api/getModulesForCourse/:courseId', async (req,res,next) => {
     return context;
 })
 
-
+// API endpoint to get the HTML for a particular course module
 router.get('/api/getModuleHTMLForCourseAndOrder/:courseId/:courseModuleId', async (req,res,next) => {
   let context = {};
   let filter = [req.params.courseId, req.params.courseModuleId]
@@ -292,6 +277,7 @@ router.get('/api/getModuleHTMLForCourseAndOrder/:courseId/:courseModuleId', asyn
     return context;
 })
 
+// API endpoint to add a course module
 router.post('/api/addCourseModule/', async (req,res) => {
   let context = {};
   let filter= [req.body.moduleOrder, req.body.moduleHTML, req.body.courseId]
@@ -306,6 +292,7 @@ router.post('/api/addCourseModule/', async (req,res) => {
   });
 });
 
+// API endpoint to delete a particular course module
 router.get('/api/deleteCourseModule/:courseModuleId', async (req,res,next) => {
   let context = {};
   mysql.pool.query("DELETE FROM `CourseModules` WHERE `courseModuleId` = ?", req.params.courseModuleId, (error, results, fields) => {
@@ -321,6 +308,7 @@ router.get('/api/deleteCourseModule/:courseModuleId', async (req,res,next) => {
   })
 });
 
+// API endpoint to update a particular course module
 router.post('/api/updateCourseModule/', async(req,res,next) => {
   let context = {};
   let filter = [req.body.newHTML, req.body.courseModuleId]
@@ -359,7 +347,7 @@ router.get('/api/getListOfAllLanguages', async (req,res,next) => {
   }
 });
 
-//add a new language
+// API endpoint to add a new language
 router.get('/api/insertLanguage/:languageName/:languageCountry', async (req,res,next) => {
   let context = {};
   let filter= [req.params.languageName, req.params.languageCountry]
@@ -375,6 +363,7 @@ router.get('/api/insertLanguage/:languageName/:languageCountry', async (req,res,
   });
 });
 
+// API endpoint to get languages assigned to a particular course
 router.get('/api/getLanguagesForCourse/:courseId', async (req,res,next) => {
   let context = {};
   mysql.pool.query(
@@ -392,8 +381,7 @@ router.get('/api/getLanguagesForCourse/:courseId', async (req,res,next) => {
     return context;
 });
 
-
-
+// API endpoint to add a particular language to a particular course
 router.get('/api/addLanguageToCourse/:courseId/:languageId', async (req,res,next) => {
   let context = {};
   let filter = [req.params.courseId, req.params.languageId]
@@ -412,6 +400,7 @@ router.get('/api/addLanguageToCourse/:courseId/:languageId', async (req,res,next
     return context;
 });
 
+// API endpoint to delete a particular language
 router.get('/api/deleteLanguage/:languageId', async (req,res,next) => {
   let context = {};
   mysql.pool.query("DELETE FROM `Languages` WHERE `languageId` = ?", req.params.languageId, (error, results, fields) => {
@@ -428,7 +417,7 @@ router.get('/api/deleteLanguage/:languageId', async (req,res,next) => {
   })
 });
 
-// Update language
+// API endpoint to update a particular language
 router.post('/api/updateLanguage/', async(req,res,next) => {
   let context = {};
   let filter = [req.body.languageName, req.body.languageCountry, req.body.languageId]
@@ -446,7 +435,7 @@ router.post('/api/updateLanguage/', async(req,res,next) => {
 //--------------------------------------------------------
 //---------------------- USERS ---------------------------
 //--------------------------------------------------------
-
+// API endpoint to delete a particular user
 router.delete('/api/deleteUser/:userId', requireAdminLogin, async (req,res,next) => {
   let context = {};
 
@@ -467,6 +456,7 @@ router.delete('/api/deleteUser/:userId', requireAdminLogin, async (req,res,next)
   }
 });
 
+// API endpoint to edit a user
 router.patch('/api/editUser', requireAdminLogin, async (req,res,next) => {
   let updates = "";
   let sql = "";
@@ -494,6 +484,7 @@ router.patch('/api/editUser', requireAdminLogin, async (req,res,next) => {
   });
 });
 
+// API endpoint to get all users and their associated type separated out into true/false values for STUDENT, INSTRUCTOR, or ADMIN
 router.get('/api/getUsersWithTypes', async (req,res,next) => {
   let context = {};
   let sqlQuery = "SELECT userId, firstName, lastName, userName, email, IF(STRCMP(userType, 'STUDENT'), false, true) AS STUDENT, IF(STRCMP(userType, 'INSTRUCTOR'), false, true) AS INSTRUCTOR, IF(STRCMP(userType, 'ADMIN'), false, true) AS ADMIN FROM Users ORDER BY lastName ASC;";
@@ -512,6 +503,7 @@ router.get('/api/getUsersWithTypes', async (req,res,next) => {
     return context;
 });
 
+// API endpoint to get students *not* enrolled in a particular class
 router.get('/api/getStudentsNotInClass/:someClassId', requireLogin, async (req,res,next) => {
   try {
     let context = {};
@@ -534,7 +526,7 @@ router.get('/api/getStudentsNotInClass/:someClassId', requireLogin, async (req,r
 //--------------------------------------------------------
 //---------------------- USERSCOURSES --------------------
 //--------------------------------------------------------
-
+// API endpoint to delete a user from a course 
 router.delete('/api/deleteUserFromCourse/:userId', requireAdminLogin, async (req,res,next) => {
   let context = {};
 
